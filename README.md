763b6448c84cbcf8094af147add082853ba9cf0f
Heaverno <935175994@qq.com>
2020-10-16 20:34:46 +0800

    final commit for first job

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # UetxAndroid
 just a file for tutorial
+apk and video, apk for etc2
+链接：https://pan.baidu.com/s/15MwlrvSjFrRTA8xBlZNjSQ 
+提取码：dbvs 
+复制这段内容后打开百度网盘手机App，操作更方便哦
---
+++ Source/UetxAndroid/UetxAndroid.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UetxAndroid.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UetxAndroid, "UetxAndroid" );
+
+DEFINE_LOG_CATEGORY(LogUetxAndroid)
+ 
